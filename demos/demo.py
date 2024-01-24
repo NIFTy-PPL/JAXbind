3a06b63065c721444e4d2cf4572ceb785e1ff4f1
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-01-24 22:00:10 +0100

    Update demo

--- demos/demo.py
+++ demos/demo.py
@@ -1,71 +1,38 @@
 import jax
 import numpy as np
 import scipy.fft
-from jax import numpy as jnp
 from jax.test_util import check_grads
 
 import jax_linop
 
 jax.config.update("jax_enable_x64", True)
 
-# def fft_operator(axes):
-#     def fft(inp, out, state):
-#         # we don't want normalization, just transpose, hence the strange norms
-#         out[()] = scipy.fft.fftn(inp, axes=state["axes"], norm="backward")
-
-#     def fft_T(inp, out, state):
-#         out[()
-#            ] = scipy.fft.ifftn(inp.conj(), axes=state["axes"],
-#                                norm="forward").conj()
-
-#     def fft_abstract(shape, dtype, state):
-#         return shape, dtype
-
-#     return jax_linop.get_linear_call(
-#         fft, fft_T, fft_abstract, fft_abstract, axes=tuple(axes)
-#     )
-
-# # create an FFT operator that transforms the first axis of its input array,
-# # as well as its adjoint operator
-# fft_op = fft_operator(axes=(0, ))
-
-# rng = np.random.default_rng(42)
-# a = rng.random((100, 20, 30)) - 0.5 + 1j * (rng.random((100, 20, 30)) - 0.5)
-
-# fft_op_T = jax.linear_transpose(fft_op, a)
-
-# check_grads(fft_op, (a, ), order=2, modes=["fwd", "rev"], eps=1.)
-# check_grads(fft_op_T, ([a], ), order=2, modes=["fwd", "rev"], eps=1.)
+from jax import numpy as jnp
+import pickle
 
 
-def fft(inp, out, state):
-    # we don't want normalization, just transpose, hence the strange norms
-    internal_fields = (
-        "_opid", "_func", "_func_T", "_func_abstract", "_func_abstract_T",
-        "_batch_axes", "_func_can_batch"
-    )
-    kw = {k: v for k, v in state.items() if k not in internal_fields}
-    batch_axes = state["_batch_axes"]
-    out[()] = scipy.fft.fftn(inp, norm="backward", **kw)
+def fft(args, out, kwargs_dump):
+    print(kwargs_dump)
+    kwargs = pickle.loads(np.ndarray.tobytes(kwargs_dump))
+    print(kwargs)
+    out[()] = args[0] + args[1]
 
 
-def fft_T(inp, out, state):
-    internal_fields = (
-        "_opid", "_func", "_func_T", "_func_abstract", "_func_abstract_T",
-        "_batch_axes", "_func_can_batch"
-    )
-    kw = {k: v for k, v in state.items() if k not in internal_fields}
-    batch_axes = state["_batch_axes"]
-    out[()] = scipy.fft.ifftn(inp.conj(), norm="forward", **kw).conj()
+def fft_T(args, out, kwargs_dump):
+    kwargs = pickle.loads(np.ndarray.tobytes(kwargs_dump))
+    out[()] = args[0] + args[1]
 
 
-def fft_abstract(shape, dtype, state):
+def fft_abstract(*args, **kwargs):
     # Returns `shape` and `dtype` of output as well as the added batch_axes of the `output``
-    in_axes = state["_batch_axes"]
-    out_axes = in_axes
-    return shape, dtype, out_axes
+    out_axes = kwargs.pop("batch_axes", ())
+    return args[0].shape, args[0].dtype, out_axes
 
 
-fft_jl = jax_linop.get_linear_call(fft, fft_T, fft_abstract, fft_abstract)
-fft_jl(jnp.zeros((2, 2, 14)).astype(jnp.complex128))
-# jax.vmap(jax.vmap(fft_jl, in_axes=0), in_axes=0)(jnp.zeros((2, 2, 14)).astype(jnp.complex128))
+fft_jl = jax_linop.get_linear_call(
+    fft, fft_T, fft_abstract, fft_abstract, func_can_batch=True
+)
+fft_jl(4 + jnp.zeros((2, 2, 14)), 1 + jnp.zeros((2, 2, 14)), axes=(3, 4))
+# fft_jl_j = jax.jit(jax.vmap(jax.vmap(fft_jl, in_axes=0), in_axes=0))
+# fft_jl_j = jax.vmap(jax.vmap(fft_jl, in_axes=0), in_axes=0)
+# fft_jl_j(jnp.zeros((2, 2, 14)).astype(jnp.complex128))
