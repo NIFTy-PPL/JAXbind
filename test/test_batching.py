e2ef851c049d70142e3a5779d82ed779c06c5ddf
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-03-09 10:36:51 +0100

    test_batching.py: Test unequal axes

--- test/test_batching.py
+++ test/test_batching.py
@@ -50,10 +50,8 @@ def f_a(*args, **kwargs):
     out_ax = [(), ()]
     if batch_axes:
         if len(batch_axes[0]) > 0 and len(batch_axes[1]) > 0:
-            ia0 = batch_axes[0][-1]
-            ia1 = batch_axes[1][-1]
-            out_ax[0] = ia0
-            out_ax[1] = ia1
+            out_ax[0] = batch_axes[0][-1]
+            out_ax[1] = batch_axes[1][-1]
         else:
             raise RuntimeError("Batching along only 1 input axis not implemented.")
     return ((x.shape, x.dtype, out_ax[0]), (y.shape, y.dtype, out_ax[1]))
@@ -81,17 +79,16 @@ rng = np.random.default_rng(42)
 a1 = rng.random((5, 5, 5)) - 0.5 + 1j * (rng.random((5, 5, 5)) - 0.5)
 a2 = rng.random((5, 5, 5)) - 0.5 + 1j * (rng.random((5, 5, 5)) - 0.5)
 a = (a1, a2)
-av1 = rng.random((5, 5, 5, 5)) - 0.5 + 1j * (rng.random((5, 5, 5, 5)) - 0.5)
-av2 = rng.random((5, 5, 5, 5)) - 0.5 + 1j * (rng.random((5, 5, 5, 5)) - 0.5)
+av1 = rng.random((5, 5, 5, 3, 4)) - 0.5 + 1j * (rng.random((5, 5, 5, 3, 4)) - 0.5)
+av2 = rng.random((5, 5, 5, 4, 3)) - 0.5 + 1j * (rng.random((5, 5, 5, 4, 3)) - 0.5)
 av = (av1, av2)
 
 
 @pmp("bt_a1", (0, 1, 2))
 @pmp("bt_a2", (0, 1, 2))
+@pmp("bt2_a1,bt2_a2", ((3, 4), (4, 3)))
 @pmp("o_a1", (0, 1))
 @pmp("o_a2", (0, 1))
-@pmp("bt2_a1", (0, 1, 2))
-@pmp("bt2_a2", (0, 1, 2))
 def test_vmap(bt_a1, bt_a2, bt2_a1, bt2_a2, o_a1, o_a2):
     vj = jax.vmap(f_jax, in_axes=(bt_a1, bt_a2), out_axes=[o_a1, o_a2])
     vb = jax.vmap(f_jax_can_batch, in_axes=(bt_a1, bt_a2), out_axes=[o_a1, o_a2])
@@ -102,11 +99,16 @@ def test_vmap(bt_a1, bt_a2, bt2_a1, bt2_a2, o_a1, o_a2):
     # check_grads(vj, a, order=2, modes=["fwd", "rev"], eps=1.0)
     # check_grads(vb, a, order=2, modes=["fwd", "rev"], eps=1.0)
 
-    vvj = jax.vmap(vj, in_axes=(bt2_a1, bt2_a2))
-    vvb = jax.vmap(vb, in_axes=(bt2_a1, bt2_a2))
+    vvj = jax.vmap(vj, in_axes=(bt2_a1, bt2_a2), out_axes=[o_a1, o_a2])
+    vvb = jax.vmap(vb, in_axes=(bt2_a1, bt2_a2), out_axes=[o_a1, o_a2])
     rb = vvb(*av)
     rj = vvj(*av)
-    np.testing.assert_allclose(rj, rb)
+    np.testing.assert_allclose(rj[0], rb[0])
+    np.testing.assert_allclose(rj[1], rb[1])
     assert rj[0].shape == rb[0].shape and rj[1].shape == rb[1].shape
     # check_grads(vvj, av, order=2, modes=["fwd", "rev"], eps=1.0)
     # check_grads(vvb, av, order=2, modes=["fwd", "rev"], eps=1.0)
+
+
+if __name__ == "__main__":
+    test_vmap(2, 2, 3, 4, 0, 1)
