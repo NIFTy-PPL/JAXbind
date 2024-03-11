ab3e5a83bdec0e85b0fe76ddf45c9e8d1c3e6435
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-03-11 14:46:05 +0100

    Set version in jax_linop and read in dynamically

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 import sys
+import os
+import codecs
 import pybind11
 from setuptools import find_packages, setup, Extension
 
 pkgname = 'jax_linop'
-version = '0.1.0'
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 include_dirs = [pybind11.get_include(True), pybind11.get_include(False)]
 
@@ -26,7 +43,7 @@ extensions = [Extension("_jax_linop",
                         extra_link_args=python_module_link_args)]
 
 setup(name=pkgname,
-      version=version,
+      version=get_version(f"{pkgname}/__init__.py"),
       packages=find_packages(include=["jax_linop"]),
       ext_modules=extensions
       )
