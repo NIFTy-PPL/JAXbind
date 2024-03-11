70db5907d84139080f6c9f92115c03a5e93458ec
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-03-11 18:35:50 +0100

    demo_nonlin.py: Rework

--- demos/demo_nonlin.py
+++ demos/demo_nonlin.py
@@ -7,7 +7,6 @@
 from functools import partial
 
 import jax
-import numpy as np
 from jax import numpy as jnp
 from jax.test_util import check_grads
 
@@ -16,120 +15,88 @@ import jaxbind
 jax.config.update("jax_enable_x64", True)
 
 
-# (x,y) -> (xy, y**2)
+# %% [markdown]
+
+# # Binding non-linear functions to JAX with fixed arguments
+
+# Bind a nonlinear function to JAX using JAXbind. Some input parts are fixed
+# (nondifferentiable) to showcase JAXbind's handling of fixed arguments.
+
+# The script begins by defining the nonlinear function using the JAXbind
+# compatible signature of out, args, and kwargs_dump. The derivative
+# function, which is registered with JAX, has the same signature as the
+# nonlinear function but includes an additional variable for the tangent of
+# the variable being differentiated. Additionally, we define the transpose of
+# the derivative function, as JAX may transpose it for operations like
+# retrieving the VJP.
+
+# %%
+
+
 def nonlin(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
+    # (x,y) -> (xy, y**2)
     x, y = args
     out[0][()] = x * y
     out[1][()] = y * y
 
 
-# (x,y,dx,dy) -> (ydx + xdy, 2 * y * dy)
 def nonlin_deriv(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
-    x, y, dx, dy = args
-    out[0][()] = y * dx + x * dy
-    out[1][()] = 2 * y * dy
-
-
-# (x, y, da, db) -> (yda, xda + 2ydb)
-def nonlin_deriv_T(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
-    x, y, da, db = args
-    out[0][()] = y * da
-    out[1][()] = x * da + 2 * y * db
-
-
-def nonlin_abstract(*args, **kwargs):
-    # Returns `shape` and `dtype` of output as well as the added batch_axes of the `output``
-    out_axes = kwargs.pop("batch_axes", ())
-    assert args[0].shape == args[1].shape
-    return (
-        (args[0].shape, args[0].dtype, out_axes),
-        (args[0].shape, args[0].dtype, out_axes),
-    )
-
-
-funcs_deriv = (nonlin_deriv, nonlin_deriv_T)
-nonlin_jax = jaxbind.get_nonlinear_call(
-    nonlin,
-    funcs_deriv,
-    nonlin_abstract,
-    nonlin_abstract,  # FIXME
-    first_n_args_fixed=0,
-    func_can_batch=True,
-)
-
-
-# inp = (4 + jnp.zeros((2, 2)), 1 + jnp.zeros((2, 2)))
-inp = (4 + jnp.zeros((1,)), 1 + jnp.zeros((1,)))
-
-check_grads(
-    partial(nonlin_jax, axes=(3, 4)), inp, order=1, modes=["fwd", "rev"], eps=1e-3
-)
-
-
-################################################## test non diff args
-# (x,y) -> (xy, y**2)
-def nonlin(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
-    x, y = args
-    out[0][()] = x * y
-    out[1][()] = y * y
-
-
-# (x,y,dy) -> (ydx + xdy, 2 * y * dy)
-def nonlin_deriv(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
+    # (x,y,dy) -> (ydx + xdy, 2 * y * dy)
     x, y, dy = args
     out[0][()] = x * dy
     out[1][()] = 2 * y * dy
 
 
-# (x, y, da, db) -> (yda, xda + 2ydb)
 def nonlin_deriv_T(out, args, kwargs_dump):
-    kwargs = jaxbind.load_kwargs(kwargs_dump)
+    # (x, y, da, db) -> (yda, xda + 2ydb)
     x, y, da, db = args
     out[0][()] = x * da + 2 * y * db
 
 
+# %% [markdown]
+
+# Define the abstract evaluation functions for JAX that translate input
+# shape and dtypes to output shape and dtypes.
+
+# %%
+
+
 def nonlin_abstract(*args, **kwargs):
-    # Returns `shape` and `dtype` of output as well as the added batch_axes of the `output``
-    out_axes = kwargs.pop("batch_axes", ())
-    assert args[0].shape == args[1].shape
-    return (
-        (args[0].shape, args[0].dtype, out_axes),
-        (args[0].shape, args[0].dtype, out_axes),
-    )
+    # Returns `shape` and `dtype` of output
+    x, y, *_ = args
+    print(f"f {_=}")
+    assert x.shape == x.shape and x.dtype is y.dtype
+    return ((x.shape, x.dtype), (x.shape, x.dtype))
 
 
 def nonlin_abstract_T(*args, **kwargs):
-    # Returns `shape` and `dtype` of output as well as the added batch_axes of the `output``
-    out_axes = kwargs.pop("batch_axes", ())
-    assert args[0].shape == args[1].shape
-    return ((args[0].shape, args[0].dtype, out_axes),)
+    # Returns `shape` and `dtype` of output
+    a, b, da, db = args
+    print(f"T {_=}")
+    assert a.shape == b.shape == da.shape == db.shape
+    assert a.dtype is b.dtype and a.dtype is da.dtype and a.dtype is db.dtype
+    return ((a.shape, a.dtype),)
 
 
-funcs_deriv = (nonlin_deriv, nonlin_deriv_T)
+# %%
 nonlin_jax = jaxbind.get_nonlinear_call(
     nonlin,
-    funcs_deriv,
+    (nonlin_deriv, nonlin_deriv_T),
     nonlin_abstract,
-    nonlin_abstract_T,  # FIXME
+    nonlin_abstract_T,
+    # Tell JAXbind that the first parameter to `nonlin` (and derived functions)
+    # is not to be differentiated.
     first_n_args_fixed=1,
     func_can_batch=True,
 )
 
-inp1 = 4 + jnp.zeros((1,))
-inp2 = 6 + jnp.zeros((1,))
+inp_f = 4 + jnp.zeros((1,))
+inp = 6 + jnp.zeros((1,))
+nonlin_jax_pt = partial(nonlin_jax, inp_f)
 
-nonlin_jax_pt = partial(nonlin_jax, inp1, axes=(3, 4))
+_ = jax.jvp(nonlin_jax_pt, (inp,), (inp,))
+_, nonlin_jax_pt_vjp = jax.vjp(nonlin_jax_pt, inp)
+nonlin_jax_pt_vjp = jax.jit(nonlin_jax_pt_vjp)
+_ = nonlin_jax_pt_vjp([inp, inp])
 
-
-check_grads(
-    partial(nonlin_jax_pt, axes=(3, 4)),
-    (inp2,),
-    order=1,
-    modes=["fwd", "rev"],
-    eps=1e-3,
-)
+check_grads(nonlin_jax_pt, (inp,), order=1, modes=("fwd", "rev"), eps=1e-3)
