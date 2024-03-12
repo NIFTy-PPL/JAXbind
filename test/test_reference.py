b752bb5ae8322c737cda443aa656098eae0446d9
Jakob Roth <roth@mpa-garching.mpg.de>
2024-03-12 09:08:54 +0100

    test_reference: turn into test

--- test/test_reference.py
+++ test/test_reference.py
@@ -11,37 +11,37 @@ import jaxbind
 
 jax.config.update("jax_enable_x64", True)
 
-
-def lin(out, args, kwargs_dump):
-    x, y = args
-    out[0][()] = x + y
-    out[1][()] = x + y
-
-
-def lin_T(out, args, kwargs_dump):
-    a, b = args
-    out[0][()] = a + b
-    out[1][()] = a + b
+def test_reference():
+    def lin(out, args, kwargs_dump):
+        x, y = args
+        out[0][()] = x + y
+        out[1][()] = x + y
 
 
-def lin_abstract(*args, **kwargs):
-    x, y = args
-    assert x.shape == y.shape
-    return ((x.shape, x.dtype), (x.shape, x.dtype))
+    def lin_T(out, args, kwargs_dump):
+        a, b = args
+        out[0][()] = a + b
+        out[1][()] = a + b
 
 
-lin_jax = jaxbind.get_linear_call(lin, lin_T, lin_abstract, lin_abstract)
-inp = (4 + jnp.zeros((2, 2)), 1 + jnp.zeros((2, 2)))
-lin_jax = jax.jit(lin_jax, static_argnames=("axes",))
-
-del lin, lin_abstract, lin_T
-
-
-def lin(*a):
-    raise ValueError()
+    def lin_abstract(*args, **kwargs):
+        x, y = args
+        assert x.shape == y.shape
+        return ((x.shape, x.dtype), (x.shape, x.dtype))
 
 
-_ = {"a": jnp.arange(10)}
+    lin_jax = jaxbind.get_linear_call(lin, lin_T, lin_abstract, lin_abstract)
+    inp = (4 + jnp.zeros((2, 2)), 1 + jnp.zeros((2, 2)))
+    lin_jax = jax.jit(lin_jax, static_argnames=("axes",))
 
-lin_jax(*inp, axes=(3, 4))
-jax.linear_transpose(lin_jax, *inp)(list(inp))
+    del lin, lin_abstract, lin_T
+
+
+    def lin(*a):
+        raise ValueError()
+
+
+    _ = {"a": jnp.arange(10)}
+
+    lin_jax(*inp, axes=(3, 4))
+    jax.linear_transpose(lin_jax, *inp)(list(inp))
