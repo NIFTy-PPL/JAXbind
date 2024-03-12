124e74c04308780266018ede40d4b685b1d4fc46
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-03-12 12:03:42 +0100

    setup.py: Format

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ import codecs
 import pybind11
 from setuptools import find_packages, setup, Extension
 
-pkgname = 'jaxbind'
+pkgname = "jaxbind"
 
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
@@ -24,26 +24,31 @@ def get_version(rel_path):
 
 include_dirs = [pybind11.get_include(True), pybind11.get_include(False)]
 
-extra_compile_args = ['-std=c++17', '-fvisibility=hidden']
+extra_compile_args = ["-std=c++17", "-fvisibility=hidden"]
 
 python_module_link_args = []
 
-if sys.platform == 'darwin':
-    extra_compile_args += ['-mmacosx-version-min=10.14']
-    python_module_link_args += ['-mmacosx-version-min=10.14']
-elif sys.platform == 'win32':
-    extra_compile_args = ['/EHsc', '/std:c++17']
+if sys.platform == "darwin":
+    extra_compile_args += ["-mmacosx-version-min=10.14"]
+    python_module_link_args += ["-mmacosx-version-min=10.14"]
+elif sys.platform == "win32":
+    extra_compile_args = ["/EHsc", "/std:c++17"]
 
-extensions = [Extension("_jaxbind",
-                        language='c++',
-                        sources=['src/_jaxbind.cc'],
-                        depends=['src/_jaxbind.cc', 'setup.py'],
-                        include_dirs=include_dirs,
-                        extra_compile_args=extra_compile_args,
-                        extra_link_args=python_module_link_args)]
+extensions = [
+    Extension(
+        "_jaxbind",
+        language="c++",
+        sources=["src/_jaxbind.cc"],
+        depends=["src/_jaxbind.cc", "setup.py"],
+        include_dirs=include_dirs,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=python_module_link_args,
+    )
+]
 
-setup(name=pkgname,
-      version=get_version(f"{pkgname}/__init__.py"),
-      packages=find_packages(include=["jaxbind"]),
-      ext_modules=extensions
-      )
+setup(
+    name=pkgname,
+    version=get_version(f"{pkgname}/__init__.py"),
+    packages=find_packages(include=["jaxbind"]),
+    ext_modules=extensions,
+)
