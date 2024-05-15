ec2a495fce03be861d7e3666cfc094b7b10d971b
Jakob Roth <roth@mpa-garching.mpg.de>
2024-05-15 10:12:10 +0200

    docs.generate: streamline

--- docs/generate.sh
+++ docs/generate.sh
@@ -9,7 +9,6 @@ for file in "${FOLDER}/01_linear_function" "${FOLDER}/02_multilinear_function" "
     jupyter-nbconvert --to markdown --execute --ExecutePreprocessor.timeout=None "${file}.ipynb"
 done
 
-FOLDER=docs/source/jaxducc
 jupytext --to ipynb "docs/source/contrib/jaxducc0.py"
 jupyter-nbconvert --to markdown "docs/source/contrib/jaxducc0.ipynb"
 
